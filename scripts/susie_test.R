06ebe3fdc6f7a1b6d61d41d318ae0189b64279f9
dariushghasemi <ghasemi.dariush@yahoo.com>
2026-08-30 23:21:10 +0200

    install susieR v0.6.6 directly from GitHub; run susie with z_method=score and check credibles

--- scripts/susie_test.R
+++ scripts/susie_test.R
@@ -1,4 +1,6 @@
 
+# Install susieR version 0.16.6
+remotes::install_github("stephenslab/susieR")
 
 #-------------------------------#
 # -----     SuSiE GWAS     -----
@@ -84,30 +86,81 @@ susieR::kriging_rss(
   R = R, n = n_GnH
 )
 
+compute_ld_from_X <- FALSE
 susie_L <- 10
 susie_iter <- 1000
 susie_min_abs_cor <- 0.5
-susie_est_resvar <- FALSE
+susie_est_resvar <- FALSE  # TRUE if using in-sample LD
+
+# Check point
+identical(sumstat$SNPID, rownames(R))
+
 
 err_handling <- function(e) { stop("❌ SuSiE failed: ", e$message) }
 
-res_rss <- tryCatch(
+res_rss_score <- tryCatch(
   susie_rss(
-    bhat = sumstat$BETA,
-    shat = sumstat$SE,
-    n = n_GnH,
+    #bhat = sumstat$BETA,
+    #shat = sumstat$SE,
+    z = z_scores,
+    z_method = "score",
+    n = n_believe,
     R = R,
     L = susie_L,
     max_iter = susie_iter,
     min_abs_corr = susie_min_abs_cor,
-    estimate_residual_variance = susie_est_resvar # TRUE if using in-sample LD
+    estimate_residual_variance = susie_est_resvar,
   ),
   error = err_handling
 )
+
+
+# SuSiE failed: The correlation matrix (7170 by 7170) is 
+# not a positive semidefinite matrix. The smallest eigenvalue 
+# is -1.26221817719385e-05. You can bypass this by "check_R = FALSE" 
+# which instead sets negative eigenvalues to 0 to allow for continued computations.
+
 
 # Extract results
 full_res <- summary(res_rss)
 cs   <- full_res$cs    # containing CS impurity indices
 vars <- full_res$vars  # containing CS Posterior Inclusion Probabilities
 
+
+
+# SuSiE Plot
+susieR::susie_plot(
+  res_rss,
+  y = "PIP",
+  b = betas,
+  xlab = "Variants",
+  add_bar = FALSE,
+  add_legend = TRUE,
+  main = paste("SeqID_locus:", my_locuseq) #, "\nmethod=score"
+)
+
+
+
+# list of the entire SNPs with PIP
+snps_pip <- vars %>%
+  transmute(
+    cs_id = cs,
+    SNPID = sumstat$SNPID[variable],
+    PIP = variable_prob
+  )
+
+# subset of GWAS results for CS variants
+cs_summary <- sumstat %>%
+  left_join(snps_pip, by = "SNPID") %>%
+  # append CS characteristics to sumstat
+  left_join(cs[1:4], join_by(cs_id == cs)) %>%
+  filter(cs_id > 0)
+
+# list of CS variants
+cs_list <- cs_summary %>%
+  summarize(
+    cs_snps = paste(SNPID, collapse = ","),
+    .by = cs_id
+  ) %>%
+  full_join(cs, join_by(cs_id == cs))
 
